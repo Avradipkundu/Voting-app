6c3444e9ec13394973666d70d77fef9ded7bd4bc
Avradipkundu <avradipkundu851@gmail.com>
2025-05-10 12:18:00 +0530

    main server

--- server.js
+++ server.js
@@ -8,11 +8,12 @@ app.use(bodyParser.json()) // req.body
 const PORT = process.env.PORT || 3000
 
 // import the router files
-
 const userRoutes = require('./routes/userRoute')
+const candidateRoutes = require('./routes/candidateRoute')
 
 // use the routers
 app.use('/user', userRoutes)
+app.use('/candidate', candidateRoutes)
 
 app.listen(PORT, () => {
     console.log("listening on port 3000")
